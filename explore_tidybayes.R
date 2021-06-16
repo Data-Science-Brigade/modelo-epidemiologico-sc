b5fecee8ab87e2348c17a93d59b444953c73431a
Jon Cardoso <jonathan.car.silva@gmail.com>
2021-06-15 21:58:00 -0300

    Add other diagnostic plots

--- explore_tidybayes.R
+++ explore_tidybayes.R
@@ -13,6 +13,7 @@ get_tidy_df <- function(model_fit, is_reported=FALSE){
   covariates <- covariates %>% as.factor() 
   
   location_names <- model_fit$stan_list$available_locations %>% as.factor()
+  location_names <- model_fit$stan_list$available_locations %>% as.factor()
   
   tidy_model <- 
     if(is_reported){
@@ -102,5 +103,70 @@ reconstruct_vars <- function(model_output, i, m, iteration){
           (base_reported$out$alpha1[iteration,,m] + base_reported$out$alpha[iteration,]))
   
   list(linear_effect=linear_effect)
+}
+
+plot_case_curve <- function(model_output, location_name){
+  dfs <- get_dfs(location_name, model_output) 
+  selected_dates <- model_output$stan_list$dates[[location_name]]
+  ggplot(dfs$data_location %>% filter(time %in% selected_dates), aes(x=time+days(6))) + 
+    geom_col(aes(y=reported_cases), fill='coral4', alpha=0.7) + 
+    geom_ribbon(aes(ymin=predicted_min2, ymax=predicted_max2), linetype="dotted", alpha=0.65, fill="deepskyblue4") + 
+    geom_ribbon(aes(ymin=predicted_min, ymax=predicted_max), linetype="dotted", alpha=0.45, fill="deepskyblue4") + 
+    ggthemes::theme_clean() + 
+    scale_x_date(date_breaks="2 weeks") + 
+    theme(axis.text.x = element_text(angle=45, hjust=1)) + 
+    xlab("Week starting on day") + ylab("Cases") + 
+    ggtitle(sprintf("Case vs Estimated infections - %s", location_name))
+}
+
+plot_death_curve <- function(model_output, location_name){
+  dfs <- get_dfs(location_name, model_output) 
+  selected_dates <- model_output$stan_list$dates[[location_name]]
+  ggplot(dfs$data_location %>% filter(time %in% selected_dates), aes(x=time+days(6))) + 
+    geom_col(aes(y=deaths), fill='coral4', alpha=0.7) + 
+    geom_ribbon(aes(ymin=death_min2, ymax=death_max2), linetype="dotted", alpha=0.65, fill="deepskyblue4") + 
+    geom_ribbon(aes(ymin=death_min, ymax=death_max), linetype="dotted", alpha=0.45, fill="deepskyblue4") + 
+    ggthemes::theme_clean() + 
+    scale_x_date(date_breaks="2 weeks") + 
+    theme(axis.text.x = element_text(angle=45, hjust=1)) + 
+    xlab("Week starting on day") + ylab("Deaths") + 
+    ggtitle(sprintf("Deaths vs Estimated deaths - %s", location_name))
+}
+
+plot_Rt_curve <- function(model_output, location_name){
+  dfs <- get_dfs(location_name, model_output) 
+  selected_dates <- model_output$stan_list$dates[[location_name]]
+  ggplot(dfs$data_location %>% filter(time %in% selected_dates), aes(x=time+days(6))) + 
+    geom_line(aes(y=rt), color='black', size=1.3) + 
+    geom_ribbon(aes(ymin=rt_min2, ymax=rt_max2), linetype="dotted", alpha=0.75, fill="seagreen") + 
+    geom_ribbon(aes(ymin=rt_min, ymax=rt_max), linetype="dotted", alpha=0.5, fill="seagreen") + 
+    ggthemes::theme_clean() + 
+    scale_x_date(date_breaks="2 weeks") + 
+    theme(axis.text.x = element_text(angle=45, hjust=1)) + 
+    xlab("Week starting on day") + ylab("Rt") + 
+    ggtitle(sprintf("R(t) - %s", location_name))
+}
+
+get_all_dfs <- function(model_output){
+  data_location <- list()
+  data_location_forecast <- list()
+  
+  i <- 1
+  for(location_name in model_output$stan_list$available_locations){
+    dfs <- get_dfs(location_name, model_output)
+    selected_dates <- model_output$stan_list$dates[[location_name]]
+    
+    data_location[[i]] <- 
+      dfs$data_location %>% 
+      filter(time %in% selected_dates) %>% 
+      mutate(index=row_number(), cum_cases=cumsum(reported_cases), cum_deaths=cumsum(deaths))
+    data_location_forecast[[i]] <- dfs$data_location_forecast
+    i <- i + 1
+  }
+  
+  dfs <- list(data_location=bind_rows(data_location),
+              data_location_forecast=bind_rows(data_location_forecast))
+  
+  dfs
 }
 
