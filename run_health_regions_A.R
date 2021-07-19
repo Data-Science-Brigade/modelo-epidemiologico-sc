9c13f712af24fddd9acce13402b84812fd866cb3
Jon Cardoso <jonathan.car.silva@gmail.com>
2021-07-19 08:09:15 -0500

    🐛 clean up scripts, there is no need to keep the model_output_all for now. Partially addresses #44

--- run_health_regions_A.R
+++ run_health_regions_A.R
@@ -37,7 +37,7 @@ micro_health_regions_B <- c("SC_RSA_LAGUNA",
 micro_health_regions <- c(micro_health_regions_A, micro_health_regions_B)
 
 for (location in micro_health_regions_A){ 
-    i <- which(micro_health_regions==location)
+    i <- which(micro_health_regions_A==location)
     option_list <- make_option_list(location,
                                     mode="FULL",
                                     default_locations_text = "all health-regions independently",
@@ -59,63 +59,4 @@ for (location in micro_health_regions_A){
     rm(model_output)    
 
 }
-
-dir_saved_models <- paste0(opt$save_path,"results/", strftime(opt$reference_date, "%Y_%m_%d") )
-this_dir = getwd()
-
-setwd(dir_saved_models)
-
-if (length(model_files) != length(micro_health_regions)){
-    sprintf("Not enough single-region models! Check if all models concluded successfully. Expected %s, got %s",
-                    length(micro_health_regions), length(model_files)    )
-    # HOW DO I PRINT AN ERROR HERE?
-    
-} else {
-    for (model_file in model_files){ 
-        i <- which(model_files==model_file)
-        load(paste0(model_file, "-stanfit.Rdata") )
-        
-        if (i==1){
-            model_output_all <- copy(model_output)
-            model_output_all$stan_list$stan_data$M <- length(micro_health_regions)
-        } else {
-            model_output_all <- update_aggregated_model(model_output_all, model_output)
-        }
-        rm(model_output)
-    }
-}
-
-model_output_all_filename <- paste0(model_output_all$filename_suffix, "-HEALTH-REG-INDEPEND-stanfit.Rdata")
-cat(sprintf("\nSaving joint model objects to %s", model_output_all_filename))
-save(model_output_all, file = model_output_all_filename)
-
-setwd(this_dir)
-
-# update covid_data in model_output_all
-covid_data_all <- read_covid_data(opt[["deaths"]], opt[["population"]], opt[["reference_date"]],
-                              allowed_locations = micro_health_regions 
-)
-
-model_output_all[["covid_data"]] <- covid_data_all
-
-make_all_three_panel_plot(model_output_all, aggregate_name = opt$aggregate_name, 
-                          save_path = opt$save_path)
-
-mi <- NULL
-wma <- NULL
-ma <- NULL
-
-make_all_forecast_plots(model_output_all, aggregate_name = opt$aggregate_name, 
-                        min_y_breaks=mi,max_y_breaks=ma, week_max_y_breaks=wma, 
-                        save_path = opt$save_path)
-
-last_8_weeks = ymd(model_output_all$reference_date_str) - 8*7 - 1
-
-make_all_C_plot(model_output_all, aggregate_name = opt$aggregate_name, min_x_break=last_8_weeks, save_path = opt$save_path)
-
-save_data_for_dashboard(model_output_all, save_path = "~/epiCataDashboard/", aggregate_name = opt$aggregate_name)
-
-print("Done")
-
-
 
